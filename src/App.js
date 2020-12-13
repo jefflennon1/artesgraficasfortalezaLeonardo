5d7a1f0aca6a538632c698b97ed24e4888152e7e
jefferson lennon <jeffersonlennon1@outlook.com>
2020-12-12 22:42:16 -0300

    adicionando parágrafos de texto

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/header'
 import './App.css';
 import Sliders from './components/sliders'
+import BodySite from './components/bodySite'
 
 export default function App(){
   return(
     <>
        <Header />
         <Sliders />
+        <BodySite />
     </>
   )
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
-import './styles.css'
+import './styles.css';
+import iconPc from './images/computador-white.png';
+import impressoa from './images/imprressora-white.png'
 
 export default function Header(){
  
   return(
     <>
       <header className="header">
-        <h2>Artes Gráficas Leonardo Moura</h2>
+        <h2>
+        <img src={impressoa}/>
+          Artes Gráficas &nbsp;&nbsp;&nbsp; 
+        <img src={iconPc}/>
+           Leonardo Moura
+        </h2>
       </header>
     </>
   )
