ea9410716bcf44e206303eee8273a452baf807fb
jefferson lennon <jeffersonlennon1@outlook.com>
2020-12-19 15:49:21 -0300

    Mais da estilização do site

--- src/components/bodySite/index.js
+++ src/components/bodySite/index.js
@@ -5,21 +5,43 @@ export default function BodySite(){
   return(
     <div className="bodySite">
       <p>
-      Contrary to popular belief, Lorem Ipsum is not simply random text.
-       It has roots in a piece of classical Latin literature from 45 BC,
-        making it over 2000 years old. Richard McClintock, a Latin professor at
-         Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-          consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-           classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil)
-             by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very po
-             pular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-              amet..", comes from a line in section 1.10.32.
-
-      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those 
-      interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are
-       also reproduced in their exact original form, accompanied by English versions from the 1914
-        translation by H. Rackham.
+      <h3>Especializado em serviços de gráfica rápida</h3>
+        <ul>
+          <li>Impressão digital de pequenos formatos – colorida e preto & branco:
+             garantindo opções e versatilidade em impressão, indo de encontro a todos os
+              tipos de necessidades e com a qualidade de off-set</li>
+           <li>
+              Cartão de visita - Seu marketing pessoal!
+                  <li>O cartão é a primeira impressão que a pessoa terá da sua empresa: 
+                    nunca esqueça disso. Um cartão pouco chamativo e feio poderá afastar 
+                    o possível cliente da sua empresa ao invés de aproximar.</li>
+                  <li>Faça seu cartão de visita voltado para seu público-alvo.</li>
+                  <li>Cartão de visita = networking: imagine que você está em uma reunião ou em algum
+                    curso que não conheça ninguém. A ideia aí é que todos troquem contatos entre si,
+                      fortalecendo o networking.</li>
+                  <li>Escolha um local de confiança para fazer seu cartão de visita: opte por uma gráfica de confiança!</li>
+          </li>  
+          <li>Calendários personalizados
+                  <li>Não resta dúvidas que estas peças gráficas são extremamente práticas, criativas e muito promocionais. Afinal de contas é possível deixar uma marca visível para os consumidores durante os 365 dias do ano.</li>
+                  <li>Para deixar este material atrativo vale criar um design diferenciado e investir em modelos variados. Logo a dica é investir nas versões com cores variadas, com Layouts únicos, mas que tenham relação com sua marca ou negócio como:</li>
+                        <li className="listaMenor">Calendário de mesa encadernado</li>
+                        <li className="listaMenor">Calendário com imã de geladeira</li>
+                        <li className="listaMenor">Calendário com postal</li>
+                        <li className="listaMenor">Calendário de bloco comercial e mini comercial</li>
+                        <li className="listaMenor">Calendário de bolso</li>
+          </li>
+          <li>Carimbos personalizados
+            <li>Há várias formas de personalizar seu carimbo de empresa, ou informações que você pode adicionar, algumas delas são:</li>
+            <li className="listaMenor">As informações da empresa</li>
+            <li className="listaMenor">A cor certa</li>
+            <li className="listaMenor">Indicar produtos em sua linha</li>
+            <li className="listaMenor">Sua assinatura</li>
+          </li>
+          <li>Encadernação
+            <li>Na hora de encadernar os materiais de estudo ou trabalho, o mais adequado é avaliar todas as técnicas de encadernação. A escolha do tipo correto garante mais praticidade, além disso, durabilidade para todos os materiais. </li>
+          </li>
+        </ul>
+    
       </p>
     </div>
   )
--- src/components/sliders/index.js
+++ src/components/sliders/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import calendar from './images/calendar.jpg';
+// import calendar from './images/calendar.jpg';
 // import card from './images/cards.png';
-import stamp from './images/stamp.jpg'
+// import stamp from './images/stamp.jpg'
 import './style.css'
 
 export default function Sliders(){
@@ -26,9 +26,12 @@ return(
     <>
      <div className="sectionDiv" >
         <div className="galeria" id="galeria">
-          <img className="selected" src={calendar} alt=""></img>
-          <img  src={stamp} alt=""></img>
+          <img  src="https://lh3.googleusercontent.com/proxy/bBY1FutfMXhWu9w4hSrvGF1FwNLv07mYs2GgIR62BIlUsaWUT3UD123stHzPqva0EUxyXeNxXEYn9gOhJCZzVmlFah95XG0EaV9nPv-fqTSJUX9MQ0TD8Mia5XwIXiFPJyy7hzYEs9_J7AhqSASH5C0pyKlYyPdN5A" alt=""></img>
+          <img className="selected" src="https://i2.wp.com/blog.360imprimir.com.br/wp-content/uploads/sites/5/2019/04/Capa-Artigo-Blog-360-1-PT-min.png?resize=1350%2C512&ssl=1" alt=""></img>
+          <img src="https://conteudo.precocerto.co/wp-content/uploads/2017/04/10-dicas-cartao-de-visita.png" alt=""/>
+          <img src="https://i.pinimg.com/originals/33/ca/62/33ca625bb4e322a1c3b4e75cb8cb06df.jpg" alt=""/>
           <span className="titleSpan" >ARTES GRÁFICAS</span>
+          <span className="titleSpan donoDoSite">Leonardo Moura</span>
         </div>
      </div>
      {start()}
